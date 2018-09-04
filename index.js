f50356721539ac9539d6179ce68993d641305753
Patrick M Geballa <patballpuck@gmail.com>
2018-09-04 22:16:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ function logWhisper(string) {
   console.log("hello")
 }
 function sayHiToGrandma(string) 
-  if(string === string.toUpperCase())
-    {return ("YES INDEED!")}
+{if(string === string.toUpperCase())
+    return ("YES INDEED!")}
   if(string === string.toLowerCase())
   {return ("I can't hear you!")}
   if(string === "I love you, Grandma.")
